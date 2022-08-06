9ef6c1e9345995da254fbc93c021dbf91c9112e4
Cyrill Vorfolomeev <firetimeloop@gmail.com>
2022-08-06 17:59:17 +0500

    Bootstrap create-react-app and Linting config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  if (true) {}
+  return (
+    <div className="App">
+      Initial
+    </div>
+  );
+}
+
+export default App;
