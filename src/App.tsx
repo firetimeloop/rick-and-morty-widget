80bc7fd46d4523254729a2b182688cdd959238b3
Cyrill Vorfolomeev <firetimeloop@gmail.com>
2022-08-09 15:54:51 +0500

    change App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import RickAndMortyWidget from './widgets/RickAndMortyWidget';
+
 
 function App() {
   return (
-    <div className="App">
-      Initial
-    </div>
+    <AppWrapper>
+      <RickAndMortyWidget/>
+    </AppWrapper>
   );
 }
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
